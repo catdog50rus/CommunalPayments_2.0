586dc996dff4c431304cf430728ed0d716af279d
Aleksei Perepelkin <catdog50rus@gmail.com>
2022-01-06 16:11:56 +0300

    #1 Add and regisred Base Di Definitions

---
+++ src/Server/CnD.CommunalPayments.Server.Api/Definitions/Base/IAppDefinitions.cs
@@ -0,0 +1,9 @@
+﻿namespace CnD.CommunalPayments.Server.Api.Definitions.Base;
+
+internal interface IAppDefinitions
+{
+    void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+
+    void ConfigureApplication(IApplicationBuilder app, IWebHostEnvironment env);
+}
+
