1f21537dbd8aab4086a857ecfedd58583ac5d08e
DESKTOP-CV9JN11\catdo <catdog50rus@gmail.com>
2022-06-10 10:08:44 +0300

    Добавлен проект для InMemory БД Добавлен базовый DbContext

---
+++ src/Server/DataLayer/DAO/CnD.CommunalPayments.Server.Dao.IMPL.InMemory/CommunalPaymentsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CnD.CommunalPayments.Server.Dao.IMPL.InMemory.Configures;
+using Microsoft.EntityFrameworkCore;
+
+namespace CnD.CommunalPayments.Server.Dao.IMPL.InMemory;
+
+public class CommunalPaymentsDbContext : DbContext
+{
+    public CommunalPaymentsDbContext(DbContextOptions<CommunalPaymentsDbContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("CommunalPaymentsInMemmoryDbase");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(InvoiceConfigure).Assembly);
+    }
+}
---
+++ src/Server/DataLayer/DAO/CnD.CommunalPayments.Server.Dao.IMPL.InMemory/Configures/InvoiceConfigure.cs
@@ -0,0 +1,29 @@
+﻿using CnD.CommunalPayments.Server.Dao.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnD.CommunalPayments.Server.Dao.IMPL.InMemory.Configures
+{
+    internal class InvoiceConfigure : IEntityTypeConfiguration<InvoiceEntity>
+    {
+        public void Configure(EntityTypeBuilder<InvoiceEntity> builder)
+        {
+            builder.ToTable("Invoices");
+            
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("id");
+            builder.Property(x => x.CreatedAt).HasColumnName("created_at");
+            builder.Property(x => x.CreatorName).HasColumnName("creator_name");
+            builder.Property(x => x.UpdatedAt).HasColumnName("updated_at");
+            builder.Property(x => x.UpdatorName).HasColumnName("updator_name");
+
+
+        }
+    }
+}
