941ce026a145008de72182cee25e495c6d7290fc
DESKTOP-CV9JN11\catdo <catdog50rus@gmail.com>
2022-06-16 08:53:06 +0300

    Реализована модель Period для БД

--- src/Server/DataLayer/DAO/CnD.CommunalPayments.Server.Dao.IMPL.InMemory/Configures/InvoiceConfigure.cs
+++ src/Server/DataLayer/DAO/CnD.CommunalPayments.Server.Dao.IMPL.InMemory/Configures/InvoiceConfigure.cs
@@ -23,3 +23,22 @@ internal class InvoiceConfigure : IEntityTypeConfiguration<InvoiceEntity>
         builder.Property(x => x.PeriodId).HasColumnName("period_id");
     }
 }
+
+internal class PeriodConfigure : IEntityTypeConfiguration<PeriodEntity>
+{
+    public void Configure(EntityTypeBuilder<PeriodEntity> builder)
+    {
+        builder.ToTable("Periods");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).HasColumnName("id");
+        builder.Property(x => x.CreatedAt).HasColumnName("created_at");
+        builder.Property(x => x.CreatorName).HasColumnName("creator_name");
+        builder.Property(x => x.UpdatedAt).HasColumnName("updated_at");
+        builder.Property(x => x.UpdatorName).HasColumnName("updator_name");
+
+        builder.Property(x => x.Month).HasColumnName("month").HasMaxLength(8);
+        builder.Property(x => x.Year).HasColumnName("year").HasMaxLength(4);
+    }
+}
